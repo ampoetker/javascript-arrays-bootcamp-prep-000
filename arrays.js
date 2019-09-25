2c2c35fb77106c780783fcd2e42fd05a1192bd1d
Amanda Poetker <amanda@strategytwelve.com>
2019-09-25 14:12:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray() {
+  var Array = [1, 2]
+  Array.unshift("foo")
+  return Array
+}
+
+function destructiveAddElementToBeginningOfArray() {
+  var Array = [1, 2]
+  Array.unshift("foo")
+  return Array
+}
